9a8b6f7f75bf655f7bc15a293ad69bfeaabd6de8
jerryzj <jerry0715no14@gmail.com>
2019-06-05 20:47:12 +0800

    Update freature extract

--- src/feature_extract.py
+++ src/feature_extract.py
@@ -7,6 +7,9 @@
 from PIL import Image
 from os import listdir, getcwd
 from os.path import isfile, isdir, join
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 import sys
 import glob
 
@@ -21,4 +24,12 @@ extension = ".jpg"
 jpg_files = list_files(extension)
 print(jpg_files)
 for file in jpg_files:
-    img = Image.open(file)
+    img = cv2.imread(file, 0)
+    feature = np.zeros_like(img)
+    orb = cv2.ORB_create()
+    kp = orb.detect(img,None)
+    kp, des = orb.compute(img, kp)
+    img2 = cv2.drawKeypoints(img,kp,feature,color=(0,255,0), flags=0)
+    print(feature)
+    plt.imshow(img2),plt.show()
+
