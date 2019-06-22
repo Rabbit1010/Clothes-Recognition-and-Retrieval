91a1cb6a75ce73199fd1aeb2bcbb8893a6e5e095
Rabbit1010 <tommyrpg1010@gmail.com>
2019-06-22 19:27:42 +0800

    Finish clothes detection

--- cloth_detection/cloth_detection.py
+++ cloth_detection/cloth_detection.py
@@ -2,17 +2,71 @@
 """
 Created on Sat Jun 22 16:01:13 2019
 
-@author: user
+@author: Wei-Hsiang, Shen
 """
 
 import numpy as np
+import time
+import tensorflow as tf
+import cv2
 import matplotlib.pyplot as plt
 
+from yolov3_tf2.models import YoloV3, YoloV3Tiny
+from utils import Read_Img_2_Tensor, Load_DeepFashion2_Yolov3, Draw_Bounding_Box
 
-def main():
-    # Code goes here
-    pass
+def Detect_Clothes(img, model_yolov3, eager_execution=True):
+    """Detect clothes in an image using Yolo-v3 model trained on DeepFashion2 dataset"""
+    img = tf.image.resize(img, (416, 416))
+
+    t1 = time.time()
+    if eager_execution==True:
+        boxes, scores, classes, nums = model_yolov3(img)
+        # change eager tensor to numpy array
+        boxes, scores, classes, nums = boxes.numpy(), scores.numpy(), classes.numpy(), nums.numpy()
+    else:
+        boxes, scores, classes, nums = model_yolov3.predict(img)
+    t2 = time.time()
+    print('Yolo-v3 feed forward: {:.2f} sec'.format(t2 - t1))
+
+    class_names = ['short_sleeve_top', 'long_sleeve_top', 'short_sleeve_outwear', 'long_sleeve_outwear',
+                  'vest', 'sling', 'shorts', 'trousers', 'skirt', 'short_sleeve_dress',
+                  'long_sleeve_dress', 'vest_dress', 'sling_dress']
+
+    # Parse tensor
+    list_obj = []
+    for i in range(nums[0]):
+        obj = {'label':class_names[int(classes[0][i])], 'confidence':scores[0][i]}
+        obj['x1'] = boxes[0][i][0]
+        obj['y1'] = boxes[0][i][1]
+        obj['x2'] = boxes[0][i][2]
+        obj['y2'] = boxes[0][i][3]
+        list_obj.append(obj)
+
+    return list_obj
+
+def Detect_Clothes_and_Crop(img_path, threshold=0.95):
+    img = Read_Img_2_Tensor(img_path)
+    model = Load_DeepFashion2_Yolov3()
+    list_obj = Detect_Clothes(img, model)
+
+    img = np.squeeze(img.numpy()) # cast tensor back to numpy array
+    img_width = img.shape[1]
+    img_height = img.shape[0]
+
+    # crop out one cloth
+    for obj in list_obj:
+        if obj['label'] == 'short_sleeve_top' and obj['confidence']>threshold:
+            img_crop = img[int(obj['y1']*img_height):int(obj['y2']*img_height), int(obj['x1']*img_width):int(obj['x2']*img_width), :]
+
+    return img_crop
 
 if __name__ == '__main__':
-    main()
+    img = Read_Img_2_Tensor('../images/test1.jpg')
+    model = Load_DeepFashion2_Yolov3()
+    list_obj = Detect_Clothes(img, model)
+    img_with_boxes = Draw_Bounding_Box(img, list_obj)
 
+    cv2.imshow("Clothes detection", cv2.cvtColor(img_with_boxes, cv2.COLOR_RGB2BGR))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.imwrite("../images/out1.jpg", cv2.cvtColor(img_with_boxes, cv2.COLOR_RGB2BGR)*255)
---
+++ cloth_detection/utils.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 16:14:17 2019
+
+@author: Wei-Hsiang, Shen
+"""
+
+import numpy as np
+import time
+import tensorflow as tf
+import cv2
+
+from yolov3_tf2.models import YoloV3
+
+
+def Draw_Bounding_Box(img, list_obj):
+    try:
+        img = img.numpy() # convert tensor to numpy array
+    except:
+        pass
+
+    img = np.squeeze(img)
+
+    img_width = img.shape[1]
+    img_height = img.shape[0]
+
+    color_yellow = [244/255, 241/255, 66/255]
+    color_green = [66/255, 241/255, 66/255]
+    color_red = [241/255, 66/255, 66/255]
+
+    # draw rectangle bounding box for cars
+    for obj in list_obj:
+        x1 = int(round(obj['x1']*img_width))
+        y1 = int(round(obj['y1']*img_height))
+        x2 = int(round(obj['x2']*img_width))
+        y2 = int(round(obj['y2']*img_height))
+
+        if obj['label'] == 'short_sleeve_top':
+            color = color_yellow
+        elif obj['label'] == 'trousers':
+            color = color_red
+        else:
+            color = color_green
+
+        text = '{}: {:.2f}'.format(obj['label'], obj['confidence'])
+
+        # draw bounding box and labels
+        img = cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+        img = cv2.putText(img, text, (x1, y1-5), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+
+    return img
+
+def Read_Img_2_Tensor(img_path):
+    img_raw = tf.io.read_file(img_path)
+    img = tf.image.decode_image(img_raw, channels=3, dtype=tf.dtypes.float32)
+    img = tf.expand_dims(img, 0) # fake a batch axis
+
+    return img
+
+def Load_DeepFashion2_Yolov3():
+    t1 = time.time()
+    model = YoloV3(classes=13)
+    model.load_weights('../built_model/deepfashion2_yolov3')
+    t2 = time.time()
+    print('Load DeepFashion2 Yolo-v3 from disk: {:.2f} sec'.format(t2 - t1))
+
+    return model
